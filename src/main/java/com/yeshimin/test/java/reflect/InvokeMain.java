c43365c9a410b0cafdfd2a1d622e0cd90f83d5d0
yeshimin <ysm@yeshimin.com>
2017-10-26 00:41:23 +0800

    #

---
+++ src/main/java/com/yeshimin/test/java/reflect/InvokeMain.java
@@ -0,0 +1,32 @@
+package com.yeshimin.test.java.reflect;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+/**
+ * Created by ysm on 17-10-26.
+ */
+public class InvokeMain {
+
+    public static void main(String[] args) {
+        try {
+            Class<?> c = Class.forName(args[0]);
+            Class[] argTypes = new Class[] { String[].class };
+            Method main = c.getDeclaredMethod("main", argTypes);
+            String[] mainArgs = Arrays.copyOfRange(args, 1, args.length);
+            System.out.format("invoking %s.main()%n", c.getName());
+            main.invoke(null, (Object)mainArgs);
+
+            // production code should handle these exceptions more gracefully
+        } catch (ClassNotFoundException x) {
+            x.printStackTrace();
+        } catch (NoSuchMethodException x) {
+            x.printStackTrace();
+        } catch (IllegalAccessException x) {
+            x.printStackTrace();
+        } catch (InvocationTargetException x) {
+            x.printStackTrace();
+        }
+    }
+}
